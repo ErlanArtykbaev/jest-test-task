3cdbbc7bdeee5c0edc5b4ccd3f24be8dabae864a
erlan artykbaev <erik.2405@mail.ru>
2020-09-01 16:49:44 +0600

    init hooks to webdriver

--- jestWebdriver.test.js
+++ jestWebdriver.test.js
@@ -7,18 +7,26 @@ describe('webdriver func', () => {
   let driver 
   let title
 
-  //this title must mutch Google
-  test('testing to google', async() => {
+  //hook before all
+  beforeAll(async () => {
     //initializing our web driver and building
     driver = await new Builder().forBrowser('firefox').build()
 
     //get request for link
     await driver.get('https://google.com')
+  })
 
+  //this title must mutch Google
+  test('testing to google', async() => {
     //geting tiitle from site we got
     title = await driver.getTitle()
 
     //matching got title with what it should be
     expect(title).toBe('Google')
+  })
+
+  //hook after all
+  afterAll(async () => {
+    await driver.quit()
   })
 })
